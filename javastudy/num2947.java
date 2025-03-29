34dd0ff8aa4ba095524b2de3ad9a2c12b196202c
seohyunk09 <2022112400@dgu.ac.kr>
2025-03-29 18:51:19 +0900

    2947번(bfs) 풀이 추가

---
+++ javastudy/num2947.java
@@ -0,0 +1,37 @@
+/* 1697번 -나무조각- 브론즈1 */
+
+package javastudy;
+
+import java.util.Scanner;
+
+public class num2947 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[5];
+		for (int i = 0; i < 5; i++) {
+			arr[i] = sc.nextInt();
+		}
+		// 두 변수의 값을 교환(swap)할 때 중간 저장 공간이 필요하기 때문<temp를 사용하는 이유>
+		int temp = 0;
+		boolean finish;
+		do {
+			finish = false;
+
+			for (int i = 0; i < arr.length - 1; i++) {
+				if (arr[i] > arr[i + 1]) {
+					temp = arr[i]; // arr[i]값을 temp에 임시 저장
+					arr[i] = arr[i + 1]; // 덮어쓰기
+					arr[i + 1] = temp; // temp=arr[i]; 여기에서 덮어 쓴 것을 다시 쓰는 것
+					finish = true;
+
+					System.out.println(arr[0] + " " + arr[1] + " " + arr[2] + " " + arr[3] + " " + arr[4]);
+				}
+			}
+
+		} while (finish);
+		sc.close();
+	}
+
+}
